5ad00dcfd22067b22091be507a934c8027000272
j_voidrem <jjmohamme14@gmail.com>
2026-05-03 04:33:18 -0400

    Simplify single-locale i18n request config

--- i18n.ts
+++ i18n.ts
@@ -1,16 +1,11 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export const locales = ['en'];
+export const locales = ['en'] as const;
+export const defaultLocale = 'en';
 
-export default getRequestConfig(async ({ requestLocale }) => {
-    let locale = await requestLocale;
-
-    if (!locale || !locales.includes(locale as any)) {
-        locale = 'en';
-    }
-
+export default getRequestConfig(async () => {
     return {
-        locale,
-        messages: (await import(`./messages/${locale}.json`)).default
+        locale: defaultLocale,
+        messages: (await import(`./messages/${defaultLocale}.json`)).default
     };
 });
