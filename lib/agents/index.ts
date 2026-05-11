58b838887d2fb269cdca5b5bccbb7ad21a6987dc
j_voidrem <jjmohamme14@gmail.com>
2026-05-10 20:09:08 -0400

    Fix Bluesky agent barrel exports

--- lib/agents/index.ts
+++ lib/agents/index.ts
@@ -1,8 +1,12 @@
-import { ProactivePostPlanner } from "./bluesky/ProactivePostPlanner";
 import { BlueskyPoster } from "./bluesky/BlueskyPoster";
 
-export { ProactivePostPlanner } from "./bluesky/ProactivePostPlanner";
 export { BlueskyPoster } from "./bluesky/BlueskyPoster";
+export {
+  logProactiveBlueskyPost,
+  planDistributionBlueskyPost,
+  planProactiveBlueskyPost,
+  updateBlueskyTopicState,
+} from "./bluesky/ProactivePostPlanner";
 
 export function createBlueskyPoster(_generateText: (prompt: string) => Promise<string>) {
   return new BlueskyPoster();
