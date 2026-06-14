c4277c8aa5e67dadbee1320614ac343313ae9b41
invidias-codem <invidias-codem@users.noreply.github.com>
2026-06-14 10:24:52 -0700

    fix: add pdf-parse type declaration to next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,3 +4,6 @@ import "./.next/types/routes.d.ts";
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
+// Type declarations for packages without built-in types
+declare module 'pdf-parse';
